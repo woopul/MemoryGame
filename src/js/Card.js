d7d4ae08ab19707a4329f6a0b38b7ae0a00b9b6f
Wojciech Opulski <wojtekopulski@gmail.com>
2019-03-27 17:36:59 +0100

    Update Card.js

--- src/js/Card.js
+++ src/js/Card.js
@@ -1,30 +1,23 @@
 import React from "react";
 
 class Card extends React.Component {
-
-
     handleClick = () => {
         //if clicked - set isFlipped on true , send this card's id up to the board component
         //If cards with this id are matched or card is flipped - function is no longer executed
-
         const {className, matched} = this.props;
         const flipped = (className === 'card flip'
             || className === 'card card-medium flip'
             || className === 'card card-hard flip');
-
+        
         if (matched === false && flipped === false) {
-
             if (typeof this.props.cardClicked === 'function') {
                 this.props.cardClicked(this.props.id, this.props.index);
             }
         }
     };
 
-
     render() {
-
         const {imgSrc, className} = this.props;
-
         return (
             <div onClick={this.handleClick} className={className}>
                 <img className='front_face' src={imgSrc} alt={imgSrc + " logo"}/>
@@ -33,5 +26,4 @@ class Card extends React.Component {
         )
     };
 }
-
 export default Card
